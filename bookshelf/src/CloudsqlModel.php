d5711612849ce2cf40dda394e0ea40ab19520ad2
Jeffrey Rennie <rennie@google.com>
2015-11-11 16:28:08 -0800

    Incomplete bookshelf sample.
    Uses CloudStorage and Cloudsql.  All tests pass.

---
+++ bookshelf/src/CloudsqlModel.php
@@ -0,0 +1,185 @@
+<?php
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Samples\Bookshelf;
+
+use PDO;
+
+/**
+ * Class CloudsqlModel implements the DataModel with a mysql database.
+ *
+ * Set the three environment variables MYSQL_DSN, MYSQL_USER,
+ * and MYSQL_PASSWORD.
+ */
+class CloudsqlModel implements DataModelInterface
+{
+    /**
+     * Creates a new PDO instance and sets error mode to exception.
+     *
+     * @return PDO
+     */
+    private function newConnection()
+    {
+        if (false == ($dsn = getenv('MYSQL_DSN'))) {
+            throw new \Exception('Set the environment variable MYSQL_DSN '.
+                'to your data source name.  See '.
+                'http://php.net/manual/en/ref.pdo-mysql.connection.php');
+        }
+        if (false == ($user = getenv('MYSQL_USER'))) {
+            throw new \Exception('Set the environment variable MYSQL_USER to '.
+                'your database user name.');
+        }
+        if (false == ($password = getenv('MYSQL_PASSWORD'))) {
+            throw new \Exception('Set the environment variable MYSQL_PASSWORD '.
+                'to your database password.');
+        }
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }
+
+    /**
+     * Creates the SQL books table if it doesn't already exist.
+     */
+    public function __construct()
+    {
+        $columns = array(
+            'id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ',
+            'title VARCHAR(255)',
+            'author VARCHAR(255)',
+            'publishedDate DATE',
+            'imageUrl VARCHAR(255)',
+            'description VARCHAR(255)',
+            'createdBy VARCHAR(255)',
+            'createdById VARCHAR(255)',
+        );
+
+        $this->columnNames = array_map(function ($columnDefinition) {
+            return explode(' ', $columnDefinition)[0];
+        }, $columns);
+        $columnText = implode(', ', $columns);
+        $pdo = $this->newConnection();
+        $pdo->query("CREATE TABLE IF NOT EXISTS books ($columnText)");
+    }
+
+    /**
+     * Throws an exception if $book contains an invalid key.
+     *
+     * @param $book array
+     *
+     * @throws \Exception
+     */
+    private function verifyBook($book)
+    {
+        if ($invalid = array_diff_key($book, array_flip($this->columnNames))) {
+            throw new \Exception(sprintf('unsupported book properties: "%s"',
+                implode(', ', $invalid)));
+        }
+    }
+
+    public function listBooks($limit = 10, $cursor = null)
+    {
+        $pdo = $this->newConnection();
+        if ($cursor) {
+            $query = 'SELECT * FROM books WHERE id > :cursor ORDER BY id'.
+                ' LIMIT :limit';
+            $statement = $pdo->prepare($query);
+            $statement->bindValue(':cursor', $cursor, PDO::PARAM_INT);
+        } else {
+            $query = 'SELECT * FROM books ORDER BY id LIMIT :limit';
+            $statement = $pdo->prepare($query);
+        }
+        $statement->bindValue(':limit', $limit + 1, PDO::PARAM_INT);
+        $statement->execute();
+        $rows = array();
+        $last_row = null;
+        $new_cursor = null;
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            if (count($rows) == $limit) {
+                $new_cursor = $last_row['id'];
+                break;
+            }
+            array_push($rows, $row);
+            $last_row = $row;
+        }
+
+        return array(
+            'books' => $rows,
+            'cursor' => $new_cursor,
+        );
+    }
+
+    public function create($book, $id = null)
+    {
+        $this->verifyBook($book);
+        if ($id) {
+            $book['id'] = $id;
+        }
+        $pdo = $this->newConnection();
+        $names = array_keys($book);
+        $placeHolders = array_map(function ($key) { return ":$key";}, $names);
+        $sql = sprintf('INSERT INTO books (%s) VALUES (%s)',
+            implode(', ', $names),
+            implode(', ', $placeHolders)
+        );
+        $statement = $pdo->prepare($sql);
+        $statement->execute($book);
+
+        return $pdo->lastInsertId();
+    }
+
+    public function read($id)
+    {
+        $pdo = $this->newConnection();
+        $statement = $pdo->prepare('SELECT * FROM books WHERE id = :id');
+        $statement->bindValue('id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function update($book)
+    {
+        $this->verifyBook($book);
+        $pdo = $this->newConnection();
+        $assignments = array_map(
+            function ($column) {return "$column=:$column";},
+            $this->columnNames
+        );
+        $assignmentString = implode(',', $assignments);
+        $sql = "UPDATE books SET $assignmentString WHERE id = :id";
+        $statement = $pdo->prepare($sql);
+        $values = array_merge(
+            array_fill_keys($this->columnNames, null),
+            $book
+        );
+        $statement->execute($values);
+
+        return $statement->rowCount();
+    }
+
+    public function delete($id)
+    {
+        $pdo = $this->newConnection();
+        $statement = $pdo->prepare('DELETE FROM books WHERE id = :id');
+        $statement->bindValue('id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->rowCount();
+    }
+}
