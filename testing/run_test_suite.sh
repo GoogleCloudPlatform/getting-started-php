1992b177a2a59952de22723ee52226773ad06efe
Takashi Matsuo <tmatsuo@google.com>
2016-02-05 15:58:33 -0800

    Enables e2e test when RUN_E2E_TEST envvar is set.

--- testing/run_test_suite.sh
+++ testing/run_test_suite.sh
@@ -24,7 +24,12 @@ php-cs-fixer fix --dry-run --diff --level=psr2 --fixers=concat_with_spaces,unuse
 
 # Run tests for each directories.
 for STEP in "${STEPS[@]}"; do
+    cp ${MYDIR}/app-e2e.yaml ${STEP}/app-e2e.yaml
     pushd ${STEP}
+    if [ ! -z ${RUN_E2E_TEST} ]; then
+        # STEP_NAME is required for the e2e tests.
+        export STEP_NAME=${STEP}
+    fi;
     mkdir -p build/logs
     php vendor/bin/phpunit --coverage-clover build/logs/clover.xml
     popd
