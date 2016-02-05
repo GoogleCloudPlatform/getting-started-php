159052980fd80f27859140d22489d15875dbd437
bshaffer <betterbrent@google.com>
2016-02-05 21:17:53 +0000

    [PUBLISHED] 59 - standardizing steps

--- 1-hello-world/test/functional/ControllersTest.php
+++ 1-hello-world/test/functional/ControllersTest.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2015 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,7 @@ namespace Google\Cloud\Samples\Bookshelf;
 use Silex\WebTestCase;
 
 /**
- * WebTestCase for the sample Hello World application.
+ * Test for application controllers
  */
 class ControllersTest extends WebTestCase
 {
