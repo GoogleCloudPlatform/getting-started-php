771f93faa7c0b624e7141a08af571340ecfe2495
Takashi Matsuo <tmatsuo@google.com>
2016-02-17 21:34:51 -0800

    Correctly clean up the firewall.

--- testing/install_test_dependencies.sh
+++ testing/install_test_dependencies.sh
@@ -93,11 +93,7 @@ else
         "${GOOGLE_APPLICATION_CREDENTIALS}"
 fi
 
-TIMESTAMP=$(date +%s)
-export FIREWALL_NAME="${FIREWALL_PREFIX}-${TIMESTAMP}"
-
 gcloud compute firewall-rules delete ${FIREWALL_NAME} || /bin/true
-sleep 10
 # Create a firewall rule for mongodb
 IP=`curl https://ip-dot-cloud-dpes.appspot.com/`
 gcloud compute firewall-rules create ${FIREWALL_NAME} \
