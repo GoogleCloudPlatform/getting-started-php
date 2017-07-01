e395a7b6e8e6f9859d60d65544f99ee57367871e
Ryan Matsumoto <ryanmats@google.com>
2017-06-30 18:59:19 -0700

    Fixed date issue

--- 2-structured-data/src/DataModel/Sql.php
+++ 2-structured-data/src/DataModel/Sql.php
@@ -42,7 +42,7 @@ class Sql implements DataModelInterface
             'id serial PRIMARY KEY ',
             'title VARCHAR(255)',
             'author VARCHAR(255)',
-            'publishedDate DATE',
+            'publishedDate VARCHAR(255)',
             'imageUrl VARCHAR(255)',
             'description VARCHAR(255)',
             'createdBy VARCHAR(255)',
