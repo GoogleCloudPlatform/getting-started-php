85c6ff3378f8e21987f94f30eb1d57f3e2b23447
Brent Shaffer <betterbrent@google.com>
2019-02-06 10:32:58 -0800

    Fixes GCE and adds tests

--- test/DeployGceTest.php
+++ test/DeployGceTest.php
@@ -17,9 +17,12 @@
 
 namespace Google\Cloud\Bookshelf;
 
+use Google\Cloud\TestUtils\DeploymentTrait;
+use Google\Cloud\TestUtils\FileUtil;
 use Google\Cloud\TestUtils\TestTrait;
-use Google\Cloud\TestUtils\AppEngineDeploymentTrait;
 use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/DeploymentTrait.php';
 
 /**
  * Class DeployTest
@@ -27,41 +30,114 @@ use PHPUnit\Framework\TestCase;
 class DeployGceTest extends TestCase
 {
     use TestTrait;
-    use AppEngineDeploymentTrait;
+    use DeploymentTrait;
 
-    private static function beforeDeploy()
-    {
-        // set "app-e2e.yaml" for app engine config
-        // set cloudsql connection name
-        $appYamlPath = __DIR__ . '/../app-e2e.yaml';
-        $appYaml = file_get_contents(__DIR__ . '/../app.yaml');
-        file_put_contents($appYamlPath, str_replace(
-            [
-                'CLOUDSQL_CONNECTION_NAME:',
-                'CLOUDSQL_DATABASE_NAME:',
-                'CLOUDSQL_USER:',
-                'CLOUDSQL_PASSWORD:',
-            ],
-            [
-                'CLOUDSQL_CONNECTION_NAME: ' . self::requireEnv('CLOUDSQL_CONNECTION_NAME'),
-                'CLOUDSQL_DATABASE_NAME: ' . self::requireEnv('CLOUDSQL_DATABASE_NAME'),
-                'CLOUDSQL_USER: ' . self::requireEnv('CLOUDSQL_USER'),
-                'CLOUDSQL_PASSWORD: ' . self::requireEnv('CLOUDSQL_PASSWORD'),
-            ],
-            $appYaml
-        ));
-    }
-
-    private static function doDeploy()
-    {
-        // deploy using "app-e2e.yaml"
-        return self::$gcloudWrapper->deploy('app-e2e.yaml');
-    }
+    private static $instanceName;
 
     public function testIndex()
     {
         $resp = $this->client->get('/books/');
         $this->assertEquals('200', $resp->getStatusCode());
         $this->assertContains('<h3>Books</h3>', (string) $resp->getBody());
+    }
+
+    public static function setUpBeforeClass()
+    {
+        // Allow setting of the instance name for testing
+        self::$instanceName = getenv('GOOGLE_INSTANCE_NAME')
+            ?: 'getting-started-' . FileUtil::randomName(4);
+    }
+
+    private static function beforeDeploy()
+    {
+        // ensure we have the environment variables we need before doing anything
+        $dbConn = self::requireEnv('CLOUDSQL_CONNECTION_NAME');
+        $dbUser = self::requireEnv('CLOUDSQL_USER');
+        $dbPass = self::requireEnv('CLOUDSQL_PASSWORD');
+
+        $tmpDir = sys_get_temp_dir() . '/test-' . FileUtil::randomName(8);
+        mkdir($tmpDir);
+        $startupScriptTmp = $tmpDir . '/startup-script.sh';
+        copy(__DIR__ . '/../gce_deployment/startup-script.sh', $startupScriptTmp);
+        echo "Copied startup-script.sh to $tmpDir\n";
+
+        // update "startup-script.sh" with the CloudSQL configuration
+        file_put_contents($startupScriptTmp, str_replace(
+            [
+                '"YOUR_CLOUDSQL_CONNECTION_NAME"',
+                '"YOUR_CLOUDSQL_USER"',
+                '"YOUR_CLOUDSQL_PASSWORD"',
+            ],
+            [
+                sprintf('"%s"', $dbConn),
+                sprintf('"%s"', $dbUser),
+                sprintf('"%s"', $dbPass),
+            ],
+            file_get_contents($startupScriptTmp)
+        ));
+
+        // set to current directory
+        chdir($tmpDir);
+    }
+
+    private static function doDeploy()
+    {
+        // Create port 80 firewall rule if it doesn't exist
+        $output = self::runGcloud('compute firewall-rules list', [
+            '--filter' => '"name~\'default-allow-http-80\'"'
+        ]);
+        if (!trim($output)) {
+            self::startGcloud('compute firewall-rules create default-allow-http-80', [
+                '--allow' => 'tcp:80',
+                '--source-ranges' => '0.0.0.0/0',
+                '--target-tags' => 'http-server',
+                '--description' => '"Allow port 80 access to http-server"',
+            ]);
+        }
+
+        // Create the instance
+        self::startGcloud(sprintf('compute instances create %s', self::$instanceName), [
+            '--image-family' => 'debian-9',
+            '--image-project' => 'debian-cloud',
+            '--machine-type' => 'g1-small',
+            '--scopes' => 'userinfo-email,cloud-platform',
+            '--metadata-from-file' => 'startup-script=startup-script.sh',
+            '--zone' => 'us-central1-f',
+            '--tags' => 'http-server',
+        ]);
+
+        // Ensure the instance is deployed and the startup script completed before continuing
+        $startTime = time();
+        $timeoutSeconds = 600;
+        $statusCmd = sprintf('compute instances get-serial-port-output %s', self::$instanceName);
+        do {
+            $output = trim(self::runGcloud($statusCmd, ['--zone' => 'us-central1-f']));
+            if (time() - $startTime > $timeoutSeconds) {
+                echo $output;
+                throw new \Exception("Startup script exceeded timeout of $timeoutSeconds seconds");
+            }
+            echo substr($output, strrpos($output, "\n", -2));
+            sleep(5);
+        } while (false === strpos($output, 'Finished running startup-script.sh'));
+    }
+
+    private static function doDelete()
+    {
+        // Delete the instance
+        $timeout = 300;
+        self::startGcloud(
+            sprintf('compute instances delete -q %s', self::$instanceName),
+            [],
+            $timeout
+        );
+    }
+
+    private static function getBaseUri()
+    {
+        $output = self::execute(sprintf(
+            "gcloud compute instances list --filter=\"name~'%s'\" | awk 'NR>1 {print $5}'",
+            self::$instanceName
+        ));
+        return 'http://' . explode("\n", $output)[0];
     }
 }
