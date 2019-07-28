5a125133c616ff07f928e1b8be9e57cf5d653246
Brent Shaffer <betterbrent@google.com>
2019-07-28 10:19:39 -0700

    report all exceptions

--- bookshelf/app/Exceptions/Handler.php
+++ bookshelf/app/Exceptions/Handler.php
@@ -13,18 +13,6 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        AuthorizationException::class,
-        HttpException::class,
-        ModelNotFoundException::class,
-        ValidationException::class,
-    ];
-
-    /**
      * Report or log an exception.
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
