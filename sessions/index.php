e8dadd90a74dec542ca2faf80c0a8ffd45ea4958
Brent Shaffer <betterbrent@google.com>
2019-09-25 14:00:30 -0400

    addresses review comments'

--- sessions/index.php
+++ sessions/index.php
@@ -1,24 +1,24 @@
 <?php
-
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 # [START getting_started_sessions_all]
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Google\Cloud\Firestore\FirestoreClient;
-use Google\Cloud\Firestore\FirestoreSessionHandler;
 
 $projectId = getenv('GOOGLE_CLOUD_PROJECT');
 # [START getting_started_sessions_register_handler]
@@ -27,9 +27,8 @@ $firestore = new FirestoreClient([
     'projectId' => $projectId,
 ]);
 
-// Configure PHP to use the the Firebase session handler.
-$handler = new FirestoreSessionHandler($firestore);
-session_set_save_handler($handler, true);
+// Configure PHP to use the the Firebase session handler.;
+session_set_save_handler($firestore->sessionHandler(), true);
 session_save_path('sessions');
 session_start();
 # [END getting_started_sessions_register_handler]
