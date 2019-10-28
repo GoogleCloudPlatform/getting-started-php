af899d77b5c88221031461709ebaa30d04f15bb9
Brent Shaffer <betterbrent@google.com>
2019-10-28 15:54:56 -0700

    misc

--- background-processing/cloud-run-backend/router.php
+++ background-processing/cloud-run-backend/router.php
@@ -8,17 +8,17 @@ require_once getenv('FUNCTION_SOURCE', true) ?: __DIR__ . '/index.php';
 (function() {
     $target = getenv('FUNCTION_TARGET', true);
     if ($target === false) {
-        trigger_error('FUNCTION_TARGET is not set');
+        throw new \Exception('FUNCTION_TARGET is not set');
     }
 
     $request = Request::createFromGlobals();
     $message = json_decode($request->getContent(), true);
     if (empty($message['message']['data'])) {
-        throw new Exception('No message received');
+        throw new \Exception('No message received');
     }
     $data = json_decode(base64_decode($message['message']['data']), true);
     if (!$data) {
-        throw new Exception('Error decoding data from message');
+        throw new \Exception('Error decoding data from message');
     }
     call_user_func_array($target, [$data]);
 })();
