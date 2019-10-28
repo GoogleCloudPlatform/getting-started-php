cc44bf69ebc0e6958e5d4c62eed0069267117acd
Brent Shaffer <betterbrent@google.com>
2019-10-28 13:09:49 -0700

    WIP

--- background-processing/worker/index.php
+++ background-processing/worker/index.php
@@ -1,14 +1,14 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-
 use Google\Cloud\Firestore\FirestoreClient;
 use Google\Cloud\Firestore\Transaction;
 use Google\Cloud\Translate\TranslateClient;
-use Symfony\Component\HttpFoundation\Request;
 
 function translateString(array $data)
 {
+    if (empty($data['language']) || empty($data['text'])) {
+        throw new Exception('Error parsing translation data');
+    }
     if (!$projectId = getenv('GOOGLE_CLOUD_PROJECT')) {
         throw new Exception('Env var GOOGLE_CLOUD_PROJECT must be set');
     }
@@ -18,10 +18,6 @@ function translateString(array $data)
     $firestore = new FirestoreClient([
         'projectId' => $projectId,
     ]);
-
-    if (empty($data['language']) || empty($data['text'])) {
-        throw new Exception('Error parsing translation data');
-    }
 
     $docId = sprintf('%s:%s', $data['language'], base64_encode($data['text']));
     $docRef = $firestore->collection('translations')->document($docId);
@@ -35,8 +31,7 @@ function translateString(array $data)
         function (Transaction $transaction) use ($translate, $translation, $docRef) {
             $snapshot = $transaction->snapshot($docRef);
             if ($snapshot->exists()) {
-                // Do nothing if the document already exists
-                return;
+                return; // Do nothing if the document already exists
             }
             $result = $translate->translate($translation['original'], [
                 'target' => $translation['lang'],
@@ -48,20 +43,3 @@ function translateString(array $data)
         }
     );
 }
-
-// Return early for tests
-if (getenv('PHPUNIT_TESTS') === '1') {
-    return;
-}
-
-$request = Request::createFromGlobals();
-$message = json_decode($request->getContent(), true);
-if (empty($message['message']['data'])) {
-    throw new Exception('No message received');
-}
-$data = json_decode(base64_decode($message['message']['data']), true);
-if (!$data) {
-    throw new Exception('Error decoding data from message');
-}
-translateString($data);
-echo "Done.";
