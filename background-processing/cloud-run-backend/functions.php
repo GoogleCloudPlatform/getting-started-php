5d591866f7f565aa036e68cc778bc9e1e66eb8c5
Brent Shaffer <betterbrent@google.com>
2019-11-15 10:53:08 -0800

    adds more region tags

--- background-processing/cloud-run-backend/functions.php
+++ background-processing/cloud-run-backend/functions.php
@@ -1,24 +1,31 @@
 <?php
 
+# [START getting_started_background_translate]
+# [START getting_started_background_translate_setup]
 use Google\Cloud\Firestore\FirestoreClient;
 use Google\Cloud\Firestore\Transaction;
 use Google\Cloud\Translate\TranslateClient;
+# [END getting_started_background_translate_setup]
 
 function translateString(array $data)
 {
     if (empty($data['language']) || empty($data['text'])) {
         throw new Exception('Error parsing translation data');
     }
-    $translate = new TranslateClient();
-    $firestore = new FirestoreClient();
 
-    $docId = sprintf('%s:%s', $data['language'], base64_encode($data['text']));
-    $docRef = $firestore->collection('translations')->document($docId);
+    # [START getting_started_background_translate_init]
+    $firestore = new FirestoreClient();
+    $translate = new TranslateClient();
 
     $translation = [
         'original' => $data['text'],
         'lang' => $data['language'],
     ];
+    # [END getting_started_background_translate_init]
+
+    # [START getting_started_background_translate_transaction]
+    $docId = sprintf('%s:%s', $data['language'], base64_encode($data['text']));
+    $docRef = $firestore->collection('translations')->document($docId);
 
     $firestore->runTransaction(
         function (Transaction $transaction) use ($translate, $translation, $docRef) {
@@ -26,15 +33,20 @@ function translateString(array $data)
             if ($snapshot->exists()) {
                 return; // Do nothing if the document already exists
             }
+
+            # [START getting_started_background_translate_string]
             $result = $translate->translate($translation['original'], [
                 'target' => $translation['lang'],
             ]);
+            # [END getting_started_background_translate_string]
             $transaction->set($docRef, $translation + [
                 'translated' => $result['text'],
                 'originalLang' => $result['source'],
             ]);
         }
     );
+    # [END getting_started_background_translate_transaction]
 
     echo "Done.";
 }
+# [END getting_started_background_translate]
